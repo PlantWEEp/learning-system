9f6811afcb3950da2af6b1c4f1a46b76622a3b7c
SonuRamesh <130997188+SonuRamesh@users.noreply.github.com>
2024-03-27 23:22:02 +0530

    Updated questionanswer route

--- src/controllers/questionanswer.js
+++ src/controllers/questionanswer.js
@@ -1,30 +1,26 @@
 const express = require('express');
 const z = require("zod");
-const questionanswer = require("../models/questionanswer.model");
+const QuestionAnswer = require("../models/questionanswer.model");
 
 const questionanswerSchema = z.object({
-    question: z.string(),
-    answer: z.string(),
-    wronganswer: z.array(z.string()), 
-    description: z.string(),
-    category: z.string(),
+    section: z.array(z.object({
+        question: z.string(),
+        answer: z.string(),
+        wronganswer: z.array(z.string()), 
+        description: z.string(),
+        category: z.string(),
+    }))
 });
 
 const studentanswers = async (req, res) => {
     try {
         const questionanswerData = questionanswerSchema.parse(req.body);
-        const { question, answer, wronganswer, description, category } = questionanswerData;
+        const { section } = questionanswerData;
 
         // Create a new question-answer document
-        const newQuestionAnswer = await questionanswer.create({
-            question,
-            answer,
-            wronganswer,
-            description,
-            category,
-        });
-
-        await newQuestionAnswer.save();
+        const newQuestionAnswer = await QuestionAnswer.create({ section });
+        
+        await newQuestionAnswer.save()
         res.status(201).json({
             success: true,
             message: 'Question-Answer pair created successfully'
@@ -38,6 +34,27 @@ const studentanswers = async (req, res) => {
     }
 };
 
+const updatequestions = async (req, res) => {
+    const id = req.params.id;
+    const newData = req.body;
+
+    try {
+        console.log('ID received:', id);
+        const updatedquestion = await QuestionAnswer.findByIdAndUpdate(id, newData,{ new: true });
+        if (!updatedquestion ) {
+            res.status(404).json({ message: 'No document found to update' });
+        } else {
+            res.json({ message: 'Document updated successfully' });
+        }
+    } catch (error) {
+        console.error('Error updating data:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+
+
 module.exports = {
-    studentanswers
+    studentanswers,
+    updatequestions
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const connectDB = require("./db/connectDB");
 const bodyParser = require("body-parser");
 const userRoutes = require('./routes/user.routes');
 const questionRoutes = require('./routes/questionanswercorrection.routes')
+const studentRoutes = require('./routes/student.routes')
 
 require('dotenv').config();
 const app = express();
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 
 app.use('/api/users', userRoutes);
 app.use('/api', questionRoutes);
+app.use('/api/student', studentRoutes);
 
 connectDB()
   .then(() => {
